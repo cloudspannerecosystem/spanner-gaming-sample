8ebfbf66f692fc9a08cb884585ca0fee812a8dc2
Derek Downey <downeyd@google.com>
2022-11-15 17:32:12 +0000

    Closes #2

---
+++ infrastructure/services.tf
@@ -0,0 +1,31 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+variable "gcp_services" {
+    description = "Enable project services"
+    type        = list(string)
+    default     = ["compute.googleapis.com", "spanner.googleapis.com", "cloudbuild.googleapis.com", "container.googleapis.com", "artifactregistry.googleapis.com"]
+}
+
+resource "google_project_service" "project_services" {
+    count = length(var.gcp_services)
+    service = var.gcp_services[count.index]
+
+    timeouts {
+        create = "30m"
+        update = "40m"
+    }
+
+    disable_dependent_services = true
+}
